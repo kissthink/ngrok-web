0a94b47fafb21e6930e2861ba6cf08dbc6f61f71
Jani Mikkonen <jani.mikkonen@gmail.com>
2014-12-05 12:52:33 +0200

    jsx transforms straight from express

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var apiRouter = require('./routes/api');
+var jsxCompile = require('express-jsx');
+
+var rootPath = path.join(__dirname, '../../dist/');
+var bowerComponents = path.join(__dirname, '../../bower_components/');
 
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.use( bodyParser.json() );
 app.use( '/api', apiRouter );
-app.use( '/', express.static(path.join(__dirname, '../../dist/')));
-app.use( '/bower_components', express.static(path.join(__dirname, '../../bower_components/')));
+app.use( jsxCompile(rootPath) );
+app.use( '/', express.static(rootPath));
+app.use( '/bower_components', express.static(bowerComponents));
 
 
 module.exports = app;
